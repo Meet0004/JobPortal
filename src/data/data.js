25dfa9f6f78207c925f5f7bab047871284c17d39
Meet0004 <meetgssoni04@gmail.com>
2025-11-14 01:01:46 +0530

    added resources

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,6 @@
 import sql from "../assets/sql.png";
+import sqlcheatsheets from "../assets/sql3.png";
+import oops from "../assets/oops.png";
 export const companiesData = [
   {
     id: 1,
@@ -21,5 +23,21 @@ export const resourcesData = [
     price: "₹9",
     link: "https://topmate.io/meet_g/1809053",
     image: sql
+  },
+  {
+    id: 2,
+    title: "SQL Notes + Cheatsheets",
+    description: "",
+    price: "₹29",
+    link: "https://topmate.io/meet_g/1809042",
+    image: sqlcheatsheets
+  },
+  {
+    id: 3,
+    title: "Java OOPs Notes",
+    description: "",
+    price: "₹9",
+    link: "https://topmate.io/meet_g/1809055",
+    image: oops
   }
 ];
