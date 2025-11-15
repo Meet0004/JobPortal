4d5e7992efb91dd58d1fc8ae5767d6a3f72fb4ae
Meet0004 <meetgssoni04@gmail.com>
2025-11-16 00:58:27 +0530

    almost done

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
     historyApiFallback: true,
     host: '0.0.0.0',
     allowedHosts: ['3b10305f2a1b.ngrok-free.app']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
   }
 })
