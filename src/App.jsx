487800f15da1370a0e5d2203af6deb613aa6d9e7
Meet0004 <meetgssoni04@gmail.com>
2025-12-01 15:02:47 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
 import CompanyDetails from './components/CompanyDetails';
